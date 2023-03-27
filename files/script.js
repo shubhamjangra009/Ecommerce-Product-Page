01abe2b7d3080b5b14e679ab269cbdfe0bfeca61
Shubham Jangra <shubhamjangra009@gmail.com>
2023-03-27 10:33:03 +0530

    absolute path changed

--- files/script.js
+++ files/script.js
@@ -1,34 +1,34 @@
 mainProductImage = document.querySelector('.main-product-image').getElementsByTagName('img')[0]
 // console.log(mainProductImage)
 
-firstSelectedImg = document.querySelector('[src="../images/image-product-1-thumbnail.jpg"]')
+firstSelectedImg = document.querySelector('[src="images/image-product-1-thumbnail.jpg"]')
 // console.log(secondSelectedImg)
 
 firstSelectedImg.addEventListener('click', () => {
     mainProductImage.removeAttribute("src")
-    mainProductImage.setAttribute("src", "../images/image-product-1.jpg")
+    mainProductImage.setAttribute("src", "images/image-product-1.jpg")
     console.log(mainProductImage)
 })
 
 
-secondSelectedImg = document.querySelector('[src="../images/image-product-2-thumbnail.jpg"]')
+secondSelectedImg = document.querySelector('[src="images/image-product-2-thumbnail.jpg"]')
 secondSelectedImg.addEventListener('click', () => {
     mainProductImage.removeAttribute("src")
-    mainProductImage.setAttribute("src", "../images/image-product-2.jpg")
+    mainProductImage.setAttribute("src", "images/image-product-2.jpg")
     console.log(mainProductImage)
 })
 
 
-thirdSelectedImg = document.querySelector('[src="../images/image-product-3-thumbnail.jpg"]')
+thirdSelectedImg = document.querySelector('[src="images/image-product-3-thumbnail.jpg"]')
 thirdSelectedImg.addEventListener('click', () => {
     mainProductImage.removeAttribute("src")
-    mainProductImage.setAttribute("src", "../images/image-product-3.jpg")
+    mainProductImage.setAttribute("src", "images/image-product-3.jpg")
     console.log(mainProductImage)
 })
 
-fourthSelectedImg = document.querySelector('[src="../images/image-product-4-thumbnail.jpg"]')
+fourthSelectedImg = document.querySelector('[src="images/image-product-4-thumbnail.jpg"]')
 fourthSelectedImg.addEventListener('click', () => {
     mainProductImage.removeAttribute("src")
-    mainProductImage.setAttribute("src", "../images/image-product-4.jpg")
+    mainProductImage.setAttribute("src", "images/image-product-4.jpg")
     console.log(mainProductImage)
 })
